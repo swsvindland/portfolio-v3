9a54bedc8d5e255b8f61090d704a0f1ab5722a98
Sam Svindland <swsvindland@gmail.com>
2024-02-24 17:05:17 -0500

    initial commit

---
+++ mdx-components.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+import { type MDXComponents } from 'mdx/types'
+import Image, { type ImageProps } from 'next/image'
+
+export function useMDXComponents(components: MDXComponents) {
+  return {
+    ...components,
+    Image: (props: ImageProps) => <Image {...props} />,
+  }
+}
---
+++ src/app/uses/page.tsx
@@ -0,0 +1,122 @@
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function ToolsSection({
+  children,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof Section>) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-16">
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Tool({
+  title,
+  href,
+  children,
+}: {
+  title: string
+  href?: string
+  children: React.ReactNode
+}) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+export const metadata = {
+  title: 'Uses',
+  description: 'Software I use, gadgets I love, and other things I recommend.',
+}
+
+export default function Uses() {
+  return (
+    <SimpleLayout
+      title="Software I use, gadgets I love, and other things I recommend."
+      intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+    >
+      <div className="space-y-20">
+        <ToolsSection title="Workstation">
+          <Tool title="16” MacBook Pro, M1 Max, 32GB RAM (2021)">
+            I was using an Intel-based 16” MacBook Pro prior to this and the
+            difference is night and day. I’ve never heard the fans turn on a
+            single time, even under the incredibly heavy loads I put it through
+            with our various launch simulations.
+          </Tool>
+          <Tool title='Samsung 27" 4k Monitor (2x)'  >
+            The only display on the market if you want something HiDPI and
+            bigger than 27”. When you’re working at planetary scale, every pixel
+            you can get counts.
+          </Tool>
+          <Tool title="Logitech MX Mecanical Mini">
+            They don’t make keyboards the way they used to. I buy these any time
+            I see them go up for sale and keep them in storage in case I need
+            parts or need to retire my main.
+          </Tool>
+          <Tool title="Logitech MX Master 3">
+            Something about all the gestures makes me feel like a wizard with
+            special powers. I really like feeling like a wizard with special
+            powers.
+          </Tool>
+          <Tool title="Amazon Basics Chair">
+            If I’m going to slouch in the worst ergonomic position imaginable
+            all day, I might as well do it in an expensive chair.
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Development tools">
+          <Tool title="Jetbrains Webstorm">
+            I don’t care if it’s missing all of the fancy IDE features everyone
+            else relies on, Sublime Text is still the best text editor ever
+            made.
+          </Tool>
+          <Tool title="Jetbrains Rider">
+            I’m honestly not even sure what features I get with this that aren’t
+            just part of the macOS Terminal but it’s what I use.
+          </Tool>
+          <Tool title="Android Sudio">
+            I’m honestly not even sure what features I get with this that aren’t
+            just part of the macOS Terminal but it’s what I use.
+          </Tool>
+          <Tool title="Xcode">
+            I’m honestly not even sure what features I get with this that aren’t
+            just part of the macOS Terminal but it’s what I use.
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Design">
+          <Tool title="Figma">
+            We started using Figma as just a design tool but now it’s become our
+            virtual whiteboard for the entire company. Never would have expected
+            the collaboration features to be the real hook.
+          </Tool>
+          <Tool title="Affinity Designer 2">
+            We started using Figma as just a design tool but now it’s become our
+            virtual whiteboard for the entire company. Never would have expected
+            the collaboration features to be the real hook.
+          </Tool>
+          <Tool title="Affinity Photo 2">
+            We started using Figma as just a design tool but now it’s become our
+            virtual whiteboard for the entire company. Never would have expected
+            the collaboration features to be the real hook.
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Productivity">
+          <Tool title="Github Copilot">
+            We started using Figma as just a design tool but now it’s become our
+            virtual whiteboard for the entire company. Never would have expected
+            the collaboration features to be the real hook.
+          </Tool>
+        </ToolsSection>
+      </div>
+    </SimpleLayout>
+  )
+}

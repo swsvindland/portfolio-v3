769386213ea9ff74df9d8c257914810d23e41b5f
Sam Svindland <swsvindland@gmail.com>
2024-09-08 12:10:58 -0400

    update dependencies

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,12 +1,7 @@
 import sgMail, { MailDataRequired } from '@sendgrid/mail'
-import { NextApiRequest, NextApiResponse } from 'next'
 import { NextResponse } from 'next/server'
 
-export const sendEmail = async (
-  email: string,
-  body: string,
-  phone?: string,
-) => {
+const sendEmail = async (email: string, body: string, phone?: string) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY ?? '')
   const msg: MailDataRequired = {
     to: process.env.SENDGRID_TO, // Change to your recipient
@@ -17,8 +12,8 @@ export const sendEmail = async (
   await sgMail.send(msg)
 }
 
-export async function POST(request: NextApiRequest, response: NextApiResponse) {
-  const { email, message, phone } = request.body
+export async function POST(request: Request) {
+  const { email, message, phone } = await request.json()
   await sendEmail(email, message, phone)
   return NextResponse.json({ message: 'Email sent' }, { status: 200 })
 }
